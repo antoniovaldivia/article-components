b536aa40123c1eb2484e77b9ed003faa42d30f43
Tracy Tran <tran-tracy@hotmail.com>
2020-02-14 08:48:49 -0500

    First steps identified

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
+// Set the display of the .progress-bar to none
 
+// Set the display of the .progress-bar to block
+
+// Store the window height
+
+// Store the document height
+
+// if document height < window height 
+
+// ..else.. statement
+
+// How much CAN be scrolled?
+
+// How much HAS been scrolled?
+
+// Determine the amount scrolled as a percentage of the total possible
+
+// Apply a width to the .progress-bar
