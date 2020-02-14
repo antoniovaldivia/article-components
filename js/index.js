d250cb1f2947f31433751b240bdad73f8ef5be35
Tracy Tran <tran-tracy@hotmail.com>
2020-02-14 09:11:42 -0500

    Complete step 2

--- js/index.js
+++ js/index.js
@@ -8,16 +8,22 @@ let $bar = document.querySelector(`.progress-bar`)
 // ********** STEPS
 
 // Set the display of the .progress-bar to none
+$bar.style.display = `none`
 
 // Set the display of the .progress-bar to block
-
+$bar.style.display = `block
+`
 // Store the window height
+let winH = $doc.clientHeight // the document's parent's height
 
 // Store the document height
+let docH = $doc.scrollHeight
 
 // if document height < window height 
+if ( docH <= winH ) { console.log(`no scroller needed`) }
 
 // ..else.. statement
+else { console.log(`I need a progress bar`) }
 
 // How much CAN be scrolled?
 
